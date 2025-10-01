9e2a7d0a1e8769be7352f276848f50d719a44081
Daivion Brooks <daivionbrooks11@gmail.com>
2025-09-30 22:30:02 -0400

    more intial setup

--- backend/models/post.py
+++ backend/models/post.py
@@ -1,2 +1,21 @@
 from app import db
 from datetime import datetime
+
+#todo: Create post model
+#todo: create rating model
+#todo: create filter models
+
+'''
+# Rating fields
+average_rating
+total_ratings
+rating_count
+created_at
+last_rated_at
+
+# Filter fields  
+category
+has_photos
+visit_count
+save_count
+'''
