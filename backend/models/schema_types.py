52179fa87a6552cbbc8775a41548fd7a6b154e6f
Daivion Brooks <daivionbrooks11@gmail.com>
2025-10-04 09:46:38 -0400

    gitignore change

---
+++ backend/models/schema_types.py
@@ -0,0 +1,23 @@
+
+
+#*USER
+user_profile_schema = 'public'
+user_info_schema = 'private'
+user_role_schema = 'private'
+user_settings_schema = 'private'
+user_subscription_schema = 'private'
+removed_user_schema = 'private'
+
+#*POST
+post_schema = 'public'
+post_media_schema = 'public'
+rating_schema = 'public'
+visit_schema = 'public'
+visit_media_schema = 'public'
+removed_post_schema = 'private'
+removed_visit_schema = 'private'
+
+#*LOCATION
+location_schema = 'public'
+location_coredata_schema = 'private'
+business_location_details_schema = 'public'
