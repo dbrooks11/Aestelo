d286846711ca6e83d55c164e41af5f677fa10cb9
Daivion Brooks <daivionbrooks11@gmail.com>
2025-12-28 00:32:04 -0500

    added accesiblity attributes to default profile photo placeholder

--- frontend/src/components/Profile/placeholder/ProfilePhotoPlaceHolder.tsx
+++ frontend/src/components/Profile/placeholder/ProfilePhotoPlaceHolder.tsx
@@ -9,8 +9,14 @@ type ProfilePhotoPlaceholderType = {
 
 export default function ProfilePhotoPlaceholder({username, className}: ProfilePhotoPlaceholderType): JSX.Element{
     return(
-        <div className={cn(className, 'dark:text-white text-black font-bold rounded-full w-full h-full flex items-center justify-center')}>
-            <span>{username.charAt(0) + username.charAt(username.length - 1)}</span>
+        <div 
+            className={cn(className, 'dark:text-white text-black font-bold rounded-full w-full h-full flex items-center justify-center pointer-events-none')}
+            role="img"
+            aria-label={`Avatar for ${username}`}
+        >
+        <span aria-hidden="true">
+            {username.charAt(0) + username.charAt(username.length - 1)}
+        </span>
         </div>
     )
 }
