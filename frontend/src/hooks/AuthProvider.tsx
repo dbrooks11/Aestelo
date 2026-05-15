926bda79cb298821ae8b8d5b47e194b22f604ace
Daivion Brooks <daivionbrooks11@gmail.com>
2026-05-15 18:11:36 -0400

    axios interceptor fix - fixed refresh interceptors that used state cookie - moved interceptor from AuthProvider to axiosHelpers - removed use effect refresh interceptor

--- frontend/src/hooks/AuthProvider.tsx
+++ frontend/src/hooks/AuthProvider.tsx
@@ -1,8 +1,6 @@
-import { useEffect, useState, type ReactNode, type JSX } from "react";
+import { useState, type ReactNode, type JSX } from "react";
 import { AuthContext, type AuthContextType } from "../context/AuthContext";
-import { csrfRefreshToken } from "../util/cookieHandlers";
-import { protectedInstance } from "../util/axiosHelpers";
-import axios from "axios";
+
 
 
 export default function AuthProvider({ children }: {children: ReactNode}): JSX.Element {
@@ -10,45 +8,6 @@ export default function AuthProvider({ children }: {children: ReactNode}): JSX.E
     const [user, setUser] = useState<boolean | null>(false);
     const [isLoading, setIsLoading] = useState<boolean>(true);
     
-
-
-    useEffect(() => {
-        const authInterceptor = protectedInstance.interceptors.response.use(
-            (response) => response,
-            async (error) => {
-                const originalRequest = error.config;
-                
-                if ([401].includes(error.response?.status) && !originalRequest._retry){
-                    originalRequest._retry = true;
-                    try{
-                        const refreshResponse = await protectedInstance.post('/auth/refresh', {}, {
-                            withCredentials: true,
-                            headers: {
-                                "X-CSRF-TOKEN": csrfRefreshToken()
-                            }
-                        })
-
-                        if (refreshResponse.status === 200){
-                            console.log("Refreshing session...")
-                            originalRequest.headers["X-CSRF-TOKEN"] = csrfRefreshToken()
-                            return axios(originalRequest)
-                        }
-                    }catch(error){
-                        console.warn("Session expired. Logging out...")
-                        setUser(false)
-                        window.location.href = '/login-email'
-                    }
-                }
-                return Promise.reject(error)
-            }
-        )
-    
-        return () => {
-            axios.interceptors.response.eject(authInterceptor)
-        }
-    }, [setUser]);
-
-
     const value: AuthContextType = {
         user,
         isLoading,
--- frontend/src/util/axiosHelpers.ts
+++ frontend/src/util/axiosHelpers.ts
@@ -1,6 +1,6 @@
 import axios, {type AxiosInstance } from "axios";
 import { appConfig } from "../config";
-import { csrfAccessToken } from "./cookieHandlers";
+import { csrfAccessToken, csrfRefreshToken } from "./cookieHandlers";
 
 
 export const protectedInstance: AxiosInstance = axios.create({
@@ -17,3 +17,30 @@ export const publicInstance: AxiosInstance = axios.create({
     withCredentials: true,
     timeout: 10000
 })
+
+protectedInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        if ([401].includes(error.response?.status) && !originalRequest._retry){
+            originalRequest._retry = true;
+            try{
+                await axios.post(`${appConfig.API_URL}/auth/refresh`, {}, {
+                    withCredentials: true,
+                    headers: {
+                        "X-CSRF-TOKEN": csrfRefreshToken()
+                    }
+                })
+                console.log("Refreshing session...")
+                const newAccessToken = csrfAccessToken()
+                protectedInstance.defaults.headers.common["X-CSRF-TOKEN"] = newAccessToken
+                originalRequest.headers["X-CSRF-TOKEN"] = newAccessToken
+                return protectedInstance(originalRequest)
+            }catch(refreshError){
+                console.warn("Session expired. Logging out...", refreshError)
+                window.location.href = '/login-email'
+            }
+        }
+        return Promise.reject(error)
+    }
+)
