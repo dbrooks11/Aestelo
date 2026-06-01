574c9c7c4327bab8e966e2a70377e63f13dc8e3a
Daivion Brooks <daivionbrooks11@gmail.com>
2026-06-01 03:51:00 -0400

    refactor(spots): separate creation endpoint into an easier background piple - Batch job to background pipline job so route returns instantly - removed apply job and map jobs

--- backend/app/routes/spot.py
+++ backend/app/routes/spot.py
@@ -1,74 +1,65 @@
 from litestar.controller import Controller
-from litestar import get, post, delete, patch, Request
+from litestar import get, post, Request
+from typing import Any
 from litestar.di import Provide
-from typing import Sequence
 from app.db.models.dto.spot import SpotDTO
 from app.db.models.services.spot import provide_spot_service, SpotService
-from app.db.models.spot import Spot, SpotMedia
-from app.schemas.spot import SpotSchemaBase, SpotInputWithMediaSchema
-from litestar.pagination import OffsetPagination
+from app.db.models.services.rating import RatingService, provide_rate_service
+from app.db.models import Spot, Rating
+from app.schemas.spot import SpotSchemaBase, SpotInputWithMediaSchema, SpotRatingSchema
+from litestar.pagination import ClassicPagination
+from litestar.params import FromPath
 from app.plugins import plugins
-from geoalchemy2.functions import ST_Point
-from app.utils.storage import storage_bb
-
+from sqlalchemy import select
+from advanced_alchemy.filters import ComparisonFilter
 
 class SpotController(Controller):
     path='/spot'
-    dependencies={'spot_service': Provide(provide_spot_service)}
+    dependencies={'spot_service': Provide(provide_spot_service), 'rate_service': Provide(provide_rate_service)}
 
 
     @get(return_dto=SpotDTO)
-    async def spot_me(self, request: Request, spot_service: SpotService) -> OffsetPagination[Spot]:
+    async def spot_me(self, request: Request, spot_service: SpotService) -> ClassicPagination[Spot]:
         user_id = request.user.id
-        return await spot_service.get_spots_me_pagination(user_id=user_id, page_size=12, page=1, sort_order='asc')
+        return await spot_service.get_spots_me_pagination(user_id=user_id, page_size=12, page=1)
 
     @post(return_dto=SpotDTO)
     async def create_spot(self, data: SpotInputWithMediaSchema, request: Request, spot_service: SpotService) -> Spot:
         user_id=request.user.id
+        post_type = 'spot'
         model_dump = data.model_dump()
-
 
         media: list[str] = model_dump.pop('media')
 
         spot = await spot_service.create_spot(user_id=user_id, data=SpotSchemaBase.model_validate(model_dump, extra='ignore'))
-        
-        queue = plugins.saq.get_queue('media_processing')
 
         tasks = []
         for order, key in enumerate(media, 1):
-                tasks.append({'post_type': 'spot', 'post_id': spot.id, 'sort_order': order, 'obj_key': key})
+            tasks.append({'sort_order': order, 'obj_key': key})
+        if tasks:
+            queue = plugins.saq.get_queue('media_processing')
 
-        media_data: list[dict[str, int | str]] = []
-        async with queue.batch():
-            try:
-                media_data = await queue.map(
-                    '',
-                    tasks,
-                    timeout=120,
-                    poll_interval=1.0,
-                    retries=3,
-                    retry_delay=1,
-                    retry_backoff=True
-                )
-                # if len(media_data) != len(media):
-                #     raise Exception('Failed to create Spot')
-                result: dict[str, int] = await queue.apply(
-                    '',
-                    data=media_data,
-                    user_id=user_id,
-                    post_id=spot.id,
-                    post_type='spot',
-                    timeout=60,
-                    poll_interval=1.0,
-                    retries=3,
-                    retry_delay=2,
-                    retry_backoff=True
-                )
-            except Exception:
-                for item in media_data:
-                    key: str = item.get('media_key') # type: ignore[attr-defined]
-                    await storage_bb.delete_file_s3(key)
-                raise
-            coordinates = ST_Point(result.get('long'), result.get('lat'))
-        return await spot_service.update_spot(data={'coordinates': coordinates}, spot_id=spot.id)
-            
+            await queue.enqueue(
+                'process_post_pipeline',
+                tasks=tasks,
+                post_id=spot.id,
+                post_type=post_type,
+                user_id=user_id,
+                timeout=60,
+                retries=2,
+                retry_delay=3,
+                retry_backoff=True
+            )
+        return spot
+        
+    @post('{spot_id:int}/rate')
+    async def rate_spot(self, spot_id: FromPath[int], data: SpotRatingSchema, rate_service: RatingService, request: Request) -> int | None:
+        user_id = request.user.id
+
+        prev_rating = await rate_service.get_rating(user_id=user_id, spot_id=spot_id)
+
+        if prev_rating and data.rating == prev_rating.rating_choice:
+            return data.rating
+
+
+        return prev_rating.rating_choice if prev_rating else None
