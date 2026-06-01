83ac2129238d6136f48cf2fad165acdadd3467a1
Daivion Brooks <daivionbrooks11@gmail.com>
2026-06-01 03:41:15 -0400

    feat(auth): implement Advanced Alchemy auth service

--- backend/app/db/models/services/auth.py
+++ backend/app/db/models/services/auth.py
@@ -1,8 +1,38 @@
 from advanced_alchemy.repository import SQLAlchemyAsyncRepository
+from advanced_alchemy.service import SQLAlchemyAsyncRepositoryService
 from sqlalchemy.ext.asyncio import AsyncSession
-from models.auth import AuthUser
+from app.db.models import (AuthUser)
+from collections.abc import AsyncGenerator
+from app.schemas.auth import AuthServiceSignupSchema
+from argon2 import PasswordHasher 
 
-class AuthRepository(SQLAlchemyAsyncRepository[AuthUser]):
+ph = PasswordHasher()
+class AuthService(SQLAlchemyAsyncRepositoryService[AuthUser]):
     """Repository for managing Authentication"""
-    model_type=AuthUser
+    class AuthRepo(SQLAlchemyAsyncRepository[AuthUser]):
+
+        model_type = AuthUser
+    
+    repository_type = AuthRepo
+
+    async def create_account(self, data: AuthServiceSignupSchema) -> None:
+        account_structure = {
+        "username": data.username,
+        "email": data.email,
+        "password_hash": ph.hash(data.password),
+        "profile": {
+            "info": {},
+            "settings": {},
+            "role": {},
+            "subscription": {},
+            "collection": [{"name": "Default", "is_default": True}]
+            }
+        }
+        await self.create(data=account_structure)
+
+async def provide_auth_service(db_session: AsyncSession) -> AsyncGenerator[AuthService, None]:
+    async with AuthService.new(session=db_session) as service:
+        yield service
+
+
 
